24ef81f98ba3c3366bead6aec42720fe45438642
YASURU2003 <yasurudanushka@gmail.com>
2026-05-04 10:47:00 +0530

    fourth commit

--- Simple API/API/app.js
+++ Simple API/API/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
-let users = ["dhanu","alex"];
-app.get("/users",(req,res)=>{
+
+app.use(express.json()); // important for POST
+
+let users = ["Dhanu", "Alex"];
+
+// GET all users
+app.get("/users", (req, res) => {
     res.json(users);
 });
 
-//post
-app.post("/users",(req,res)=>{
-    const newuser = req.body.name;
-    users.push(newuser);
-    res.send("used added");
-});s
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
+// POST new user
+app.post("/users", (req, res) => {
+    const newUser = req.body.name;
+    users.push(newUser);
+    res.send("User added!");
 });
 
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
